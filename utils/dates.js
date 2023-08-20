3cb6f6e72dd8c41fb8f4ae14d1e7c6a84056e917
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-20 16:06:01 +0330

    Helper function to get today's date

---
+++ utils/dates.js
@@ -0,0 +1,29 @@
+import { format } from "date-fns-jalali";
+import moment from "moment";
+import momentHijri from "moment-hijri";
+
+import convertToFarsi from "./numberConversion";
+
+// Today's date
+const today = new Date();
+
+// Get today's date in Gregorian
+export const getGregorianToday = () => {
+  return moment(today).format("D MMMM, YYYY");
+};
+
+// Get today's date in Jalali
+export const getJalaliToday = () => {
+  const brief = format(today, "yyyy/MM/dd");
+  const verbose = convertToFarsi(format(today, "eeee، d MMMM yyyy"));
+
+  return {
+    brief,
+    verbose,
+  };
+};
+
+// Get today's date in Hijri
+export const getHijriToday = () => {
+  return convertToFarsi(momentHijri(today).format("iD iMMMM، iYYYY"));
+};
