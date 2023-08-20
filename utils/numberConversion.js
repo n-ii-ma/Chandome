d89571491a3370a4e9e0c5006e2f05887622236b
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-20 14:54:52 +0330

    Convert numbers to farsi

---
+++ utils/numberConversion.js
@@ -0,0 +1,5 @@
+export default function convertToFarsi(n) {
+  const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+
+  return n.toString().replace(/\d/g, (x) => farsiDigits[x]);
+}
