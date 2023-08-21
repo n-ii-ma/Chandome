046f511eba40aabde0c22ebce9c5568b151523c9
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-08-21 11:55:21 +0330

    Hide Splash screen when app is ready to render

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
+import "react-native-reanimated";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, ImageBackground } from "react-native";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import * as SplashScreen from "expo-splash-screen";
 
 import Date from "./components/Date";
@@ -11,7 +12,7 @@ import {
   getHijriToday,
 } from "./utils/dates";
 
-// Keep the splash screen visible while we fetch resources
+// Keep the splash screen visible until resources are fetched
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
@@ -19,35 +20,50 @@ const App = () => {
   const [jalaliToday, setJalaliToday] = useState(null); // Today in Jalali
   const [hijriToday, setHijriToday] = useState(null); // Today in Hijri
   const [isHoliday, setIsHoliday] = useState(false); // Is today a holiday
+  const [appIsReady, setAppIsReady] = useState(false); // Is app ready to render
 
   // Load data
   useEffect(() => {
-    setGregorianToday(getGregorianToday());
-    setJalaliToday(getJalaliToday().verbose);
-    setHijriToday(getHijriToday());
+    async function prepare() {
+      // Load dates
+      setGregorianToday(getGregorianToday());
+      setJalaliToday(getJalaliToday().verbose);
+      setHijriToday(getHijriToday());
 
-    isHolidayAsync();
+      // Send request to check if today is a Jalali holiday
+      try {
+        const response = await fetch(
+          `https://holidayapi.ir/jalali/${getJalaliToday().brief}`
+        );
+        const data = await response.json();
+
+        // Set is holiday state based on response
+        if (data?.is_holiday) {
+          setIsHoliday(true);
+        } else {
+          setIsHoliday(false);
+        }
+      } catch (error) {
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
 
-  // Send request to check if today is a Jalali holiday
-  const isHolidayAsync = async () => {
-    try {
-      const response = await fetch(
-        `https://holidayapi.ir/jalali/${getJalaliToday().brief}`
-      );
-      const data = await response.json();
-
-      // Set is holiday state based on response
-      if (data?.is_holiday) {
-        setIsHoliday(true);
-      } else {
-        setIsHoliday(false);
-      }
-    } catch (error) {
-    } finally {
+  // Hide the splash screen after the root view has already performed layout
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
       await SplashScreen.hideAsync();
     }
-  };
+  }, [appIsReady]);
+
+  // Return null if app is not ready to render
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
     <ImageBackground
@@ -55,6 +71,7 @@ const App = () => {
       style={styles.container}
       resizeMode="cover"
       alt="Black sand dunes"
+      onLayout={onLayoutRootView}
     >
       <Date
         gregorianToday={gregorianToday}
