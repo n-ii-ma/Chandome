5d1897f5754bb7b0bfc14f04894c39114bfcdba5
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-09-11 13:00:51 +0330

    Specify types

---
+++ types.ts
@@ -0,0 +1,38 @@
+export interface DateProps {
+  /**
+   * Today's date in Gregorian calendar.
+   */
+  gregorianToday: string;
+
+  /**
+   * Today's date in Jalali calendar.
+   */
+  jalaliToday: string;
+
+  /**
+   * Today's date in Hijri calendar.
+   */
+  hijriToday: string;
+
+  /**
+   * Whether today is a Jalali holiday or not.
+   */
+  isHoliday: boolean;
+
+  /**
+   * Jalali holiday description.
+   */
+  holidayDesc: string;
+}
+
+export interface GetJalaliTodayArgs {
+  /**
+   * Today in Jalali calendar with a yyyy/mm/dd format.
+   */
+  brief: string;
+
+  /**
+   * Today in Jalali calendar with a detailed format.
+   */
+  verbose: string;
+}
