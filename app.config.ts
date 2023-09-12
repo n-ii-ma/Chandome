c45168f6bcb7528622e6ffb231692450ab3d9f4b
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-09-12 10:12:12 +0330

    Upgrade app version

--- app.config.ts
+++ app.config.ts
@@ -32,7 +32,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   description:
     "An App which Gives Today's Date in Gregorian, Jalali, and Hijri",
   slug: "Chandome",
-  version: "4.0.0",
+  version: "5.0.0",
   orientation: "portrait",
   icon: "./assets/icons/icon.png",
   userInterfaceStyle: "light",
@@ -56,7 +56,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       monochromeImage: "./assets/icons/icon_monochrome.png",
     },
     package: "com.nima96.Chandome",
-    versionCode: 4,
+    versionCode: 5,
   },
   web: {
     favicon: "./assets/icons/favicon.png",
