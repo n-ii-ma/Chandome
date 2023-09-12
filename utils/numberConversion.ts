1947902156ca11c69304925b3a2f4e533cf6d54a
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2023-09-12 09:44:00 +0330

    Fix TypeScript errors

--- utils/numberConversion.ts
+++ utils/numberConversion.ts
@@ -1,6 +1,18 @@
 /** Convert numbers to Farsi. */
 export default function convertToFarsi(str: string): string {
-  const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  // Type *Index Signature*
+  const farsiDigits: { [index: string]: any } = [
+    "۰",
+    "۱",
+    "۲",
+    "۳",
+    "۴",
+    "۵",
+    "۶",
+    "۷",
+    "۸",
+    "۹",
+  ];
 
   return str.replace(/\d/g, (x) => farsiDigits[x]);
 }
