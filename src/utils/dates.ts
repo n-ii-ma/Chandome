1a2d32c3a95190cf8ac7e72087ba2563707d3145
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2026-07-07 11:42:17 +0330

    chore: replace `moment` and `moment-hijri` with `hijri-converter` to increase performance

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,6 +1,5 @@
+import { gregorianToHijri } from "@tabby_ai/hijri-converter";
 import { format } from "date-fns-jalali";
-import moment from "moment";
-import momentHijri from "moment-hijri";
 
 import type { GetJalaliTodayArgs } from "@/global/types";
 
@@ -8,8 +7,43 @@ import convertToFarsi from "./numberConversion";
 
 const today = new Date();
 
-export const getGregorianToday = (): string =>
-  moment(today).format("D MMMM, YYYY");
+const GREGORIAN_MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const HIJRI_MONTHS = [
+  "محرم",
+  "صفر",
+  "ربیع‌الاول",
+  "ربیع‌الثانی",
+  "جمادی‌الاول",
+  "جمادی‌الثانی",
+  "رجب",
+  "شعبان",
+  "رمضان",
+  "شوال",
+  "ذی‌القعده",
+  "ذی‌الحجه",
+];
+
+export const getGregorianToday = (): string => {
+  const year = today.getFullYear();
+  const month = GREGORIAN_MONTHS[today.getMonth()];
+  const day = today.getDate();
+
+  return `${month} ${day}, ${year}`;
+};
 
 export const getJalaliToday = (): GetJalaliTodayArgs => {
   const brief = format(today, "yyyy/MM/dd");
@@ -21,5 +55,12 @@ export const getJalaliToday = (): GetJalaliTodayArgs => {
   };
 };
 
-export const getHijriToday = (): string =>
-  convertToFarsi(momentHijri(today).format("iD iMMMM، iYYYY"));
+export const getHijriToday = (): string => {
+  const { year, month, day } = gregorianToHijri({
+    year: today.getFullYear(),
+    month: today.getMonth() + 1, // Month number in Javascript Date API is zero-based
+    day: today.getDate(),
+  });
+
+  return convertToFarsi(`${day} ${HIJRI_MONTHS[month - 1]}، ${year}`);
+};
