3fb89d81042511f2c683c9fe19eef5f02cd7896c
n-ii-ma <88039431+n-ii-ma@users.noreply.github.com>
2026-07-08 09:42:47 +0330

    refactor: use `Expo Image` instead of `ImageBackground`

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,5 +1,6 @@
 import * as Application from "expo-application";
-import { ImageBackground, StyleSheet, Text } from "react-native";
+import { Image } from "expo-image";
+import { StyleSheet, Text, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { ms } from "@/utils/scaling";
@@ -12,32 +13,37 @@ const Background = ({ isHoliday, holidayDesc }: BackgroundProps) => {
   const insets = useSafeAreaInsets();
 
   return (
-    <ImageBackground
-      source={require("../../assets/images/black_sand_dunes.jpg")}
-      resizeMode="cover"
-      alt="Black sand dunes"
-      style={[
-        styles.container,
-        {
-          paddingTop: insets.top,
-          paddingBottom: insets.bottom,
-          paddingLeft: insets.left,
-          paddingRight: insets.right,
-        },
-      ]}
-    >
-      <Date isHoliday={isHoliday} holidayDesc={holidayDesc} />
-      <Text style={[styles.versionTxt, { bottom: insets.bottom }]}>
-        v{Application.nativeApplicationVersion}
-      </Text>
-    </ImageBackground>
+    <>
+      <Image
+        source={require("../../assets/images/black_sand_dunes.jpg")}
+        contentFit="cover"
+        alt="Black sand dunes"
+        style={StyleSheet.absoluteFill}
+      />
+      <View
+        style={[
+          styles.content,
+          {
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingLeft: insets.left,
+            paddingRight: insets.right,
+          },
+        ]}
+      >
+        <Date isHoliday={isHoliday} holidayDesc={holidayDesc} />
+        <Text style={[styles.versionTxt, { bottom: insets.bottom }]}>
+          v{Application.nativeApplicationVersion}
+        </Text>
+      </View>
+    </>
   );
 };
 
 export default Background;
 
 const styles = StyleSheet.create({
-  container: {
+  content: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
